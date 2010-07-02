65d30b88e993375b063999f7d29d846c8dc4028d
kouno <kouno@iugonet1.stelab.nagoya-u.ac.jp>
2010-07-02 17:51:23 +0900

    modified bugs

--- clean.sh
+++ clean.sh
@@ -3,3 +3,8 @@
 rm -rf ImportData_*
 rm -rf ReplaceData_*
 rm -rf DeleteData
+rm -rf runImport.sh
+rm -rf runReplace.sh
+rm -rf runDelete.sh
+rm -rf *.xml
+rm -rf *.log
--- main2/ScriptMaker.rb
+++ main2/ScriptMaker.rb
@@ -3,17 +3,17 @@
 class ScriptMaker
 
  def initialize( filename )
-   fw = open( filename, "w" )
-   fw.puts "#!/bin/bash"
-   fw.puts ""
+   @fw = open( filename, "w" )
+   @fw.puts "#!/bin/bash"
+   @fw.puts ""
  end
 
  def puts( str )
-   fw.puts str
+   @fw.puts str
  end
 
  def finalize()
-   fw.close
+   @fw.close
  end
 
 end
