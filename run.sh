c68b595c7418a41378f372990efe6a64fc8e0b7f
kouno <kouno@iugonet1.stelab.nagoya-u.ac.jp>
2010-11-12 19:10:17 +0900

    minor modification

--- run.sh
+++ run.sh
@@ -2,26 +2,21 @@
 
 export RUBYLIB=.:$RUBYLIB
 
-
-rm -f i.out
-rm -f update.out
 date
-pwd
 
 IMPORT_PID=`pgrep -u dspace -o run.sh`
 echo $IMPORT_PID
 
 if [ $$ = $IMPORT_PID ]; then
+ rm -f update.out
  ruby main.rb >& i.out
  if [ -s FileStatus.out ]; then
    /opt/dspace/bin/index-update
  fi
-  ./clean.sh
+ ./clean.sh
+ cp update.out     /opt/dspace/webapps/iugonet/iugonet/.
 else
  echo "PASS"
 fi
 
 date
-cp i.out          /opt/dspace/webapps/iugonet/iugonet/.
-cp update.out     /opt/dspace/webapps/iugonet/iugonet/.
-
